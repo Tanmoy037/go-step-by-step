474fcb8334e77fbb18788fb6b467fd2985d4fddf
Tanmoy Santra <108757431+Tanmoy037@users.noreply.github.com>
2023-06-28 23:42:32 +0530

    Update variables.tf

--- todo/variables.tf
+++ todo/variables.tf
@@ -1,18 +1,9 @@
-variable "aws_access_key" {
-  description = "Your AWS access key"
-  default = env("AWS_ACCESS_KEY_ID")
+variable "access_key" {
+    description = "Access key to AWS console"
 }
-
-variable "aws_secret_key" {
-  description = "Your AWS secret key"
-  default = env("AWS_SECRET_ACCESS_KEY")
+variable "secret_key" {
+    description = "Secret key to AWS console"
 }
-
-
 variable "region" {
-  description = "The AWS region"
-  default = "ap-south-1"
+    description = "AWS region"
 }
-
-# export AWS_ACCESS_KEY_ID=my-access-key-id
-# export AWS_SECRET_ACCESS_KEY=my-secret-access-key
