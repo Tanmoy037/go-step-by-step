3cbec6f6fc7c0506114b013f9d3dd101c1ff9e53
Tanmoy Santra <tanmoysantra67@gmail.com>
2023-06-29 18:45:08 +0530

    syntax error fixed

--- todo/main.tf
+++ todo/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region     = "${var.region}"
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
-
+}
 resource "aws_rds_instance" "my_instance" {
   engine = "mysql"
   identifier = "myrdsinstance"
