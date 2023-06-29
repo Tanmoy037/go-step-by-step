215b417126e0fcdb283ac360ccadb8c14cbb3376
Tanmoy Santra <tanmoysantra67@gmail.com>
2023-06-29 21:36:25 +0530

    script for security group added

--- todo/outputs.tf
+++ todo/outputs.tf
@@ -1,3 +1,6 @@
-output "db_instance_endpoint" {
+output "security_group_id" {
+  value  = aws_security_group.rds_sg.id
+}
+git output "db_instance_endpoint" {
   value = aws_db_instance.my_instance.endpoint
 }
