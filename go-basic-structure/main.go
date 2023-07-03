5f7b0ebaed323cb648f9015451f1f3f502478313
Tanmoy Santra <tanmoysantra67@gmail.com>
2023-07-03 21:49:05 +0530

    structs added

--- go-basic-structure/main.go
+++ go-basic-structure/main.go
@@ -9,35 +9,33 @@ import (
 
 	"time"
 )
-func add(x,y int) int {
-	return x+y
+
+func add(x, y int) int {
+	return x + y
 }
 
-func main(){
+func main() {
 	//variable
 	var name = "Tanmoy"
 	fmt.Println("Hello from Go")
-	fmt.Println("Hello",name)
+	fmt.Println("Hello", name)
 	//for loop
-	for i := 0; i<5; i++ {
+	for i := 0; i < 5; i++ {
 		fmt.Println(i)
 	}
 	// if else
 	x := 10
 	if x < 5 {
 		fmt.Println("less than 5")
-	}else{
+	} else {
 		fmt.Println("greater than 5")
 	}
 	// function
 
+	result := add(4, 6)
+	fmt.Println(result)
 
-
-		result := add(4,6)
-		fmt.Println(result)
- 
-
-	// taking input from user 
+	// taking input from user
 	// reader := bufio.NewReader(os.Stdin)
 	// fmt.Println("Enter your name: ")
 
@@ -48,10 +46,10 @@ func main(){
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter you rating for our app 1 to 5: ")
 	input, _ := reader.ReadString('\n')
-	fmt.Println("Thanks for rating ",input)
-	fmt.Printf("Type of this rating is %T",input)
+	fmt.Println("Thanks for rating ", input)
+	fmt.Printf("Type of this rating is %T", input)
 
-	numRating, err := strconv.ParseFloat(strings.TrimSpace(input),64)
+	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
 
 	if err != nil {
 		fmt.Println(err)
@@ -66,13 +64,12 @@ func main(){
 
 	// Go build (Need to run in terminal)
 	// GOOS="windows" go build
-	// GOOS="linux" go build	
-
+	// GOOS="linux" go build
 
 	// pointer
 
 	myNumber := 23
-	
+
 	var ptr = &myNumber
 
 	fmt.Println("Address of myNumber is: ", ptr)
@@ -83,21 +80,19 @@ func main(){
 	fmt.Println("New value of myNumber is: ", myNumber)
 
 	//array
-	// var fruitList[4]string	
+	// var fruitList[4]string
 
 	// fruitList[0] = "Apple"
 	// fruitList[1] = "Banana"
 	// fruitList[2] = "Orange"
-	
+
 	// fmt.Println(fruitList)
 
-
-
 	// slices
-	var fruitList = []string{"Apple","Banana","Orange"}
-	fmt.Printf("Type of fruitlist is %T\n",fruitList)
+	var fruitList = []string{"Apple", "Banana", "Orange"}
+	fmt.Printf("Type of fruitlist is %T\n", fruitList)
 
-	fruitList = append(fruitList,"Mango","Banana")
+	fruitList = append(fruitList, "Mango", "Banana")
 	fmt.Println(fruitList)
 
 	fruitList = append(fruitList[1:3])
@@ -107,10 +102,10 @@ func main(){
 
 	//how to remove a value from a slice based on index
 
-	var courses = []string{"Go","Python","Java","C++","ruby"}
+	var courses = []string{"Go", "Python", "Java", "C++", "ruby"}
 	fmt.Println(courses)
 	var index int = 2
-	courses = append(courses[:index],courses[index+1:]...)
+	courses = append(courses[:index], courses[index+1:]...)
 	fmt.Println(courses)
 
 	// Maps in golang
@@ -123,13 +118,29 @@ func main(){
 	languages["JS"] = "Javascript"
 
 	fmt.Println(languages)
-	fmt.Println("Js shorts for: ",languages["JS"])
-	delete(languages,"JS") 
+	fmt.Println("Js shorts for: ", languages["JS"])
+	delete(languages, "JS")
 
 	// loops using map
 
-	for key,value := range languages {
-		fmt.Printf("For key %v, value is %v\n",key,value)
+	for key, value := range languages {
+		fmt.Printf("For key %v, value is %v\n", key, value)
 	}
 	
+	// Structs in golang
+
+	fmt.Println("Structs in golang")
+	// no inheritance in golang; No super or paren
+	tanmoy := User{"Tanmoy", "tanmoy@go.dev", true, 23}
+	fmt.Println(tanmoy)
+	fmt.Printf("Tanmoy details are: %+v\n", tanmoy)
+}	
+type User struct {
+	name   string
+	Email  string
+	Status bool
+	Age    int
 }
+	
+
+
