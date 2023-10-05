26d7135370f54d21e00d3b52ce2cde384a62be40
Tanmoy037 <tanmoysantra67@gmail.com>
2023-10-05 23:18:40 +0530

    setting up mongodb with api

---
+++ mongoapi/controller/controller.go
@@ -0,0 +1,9 @@
+package controller
+
+
+const connectionString = "mongodb+srv://tanmoysantra67:Tanmoy2000@cluster0.ozd0zhu.mongodb.net/?retryWrites=true&w=majority"
+const dbName = "netflix"
+const colName = "watchlist"
+
+// MOST IMPORTANT
+var collection *mongo.Collection
