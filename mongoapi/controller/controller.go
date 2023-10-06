f1defffc6205728974fff4e92a752d2b17a337ba
Tanmoy037 <tanmoysantra67@gmail.com>
2023-10-06 19:58:54 +0530

    connection created and data inserted

--- mongoapi/controller/controller.go
+++ mongoapi/controller/controller.go
@@ -1,8 +1,51 @@
 package controller
 
+import (
+	"context"
+	"fmt"
+	"log"
 
-const connectionString = " "
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const connectionString = "mongodb+srv://tanmoysantra67:<password>@cluster0.ozd0zhu.mongodb.net/?retryWrites=true&w=majority"
 const colName = "watchlist"
 
 // MOST IMPORTANT
 var collection *mongo.Collection
+
+// connect with mongoDB
+
+func init() {
+	//client option
+	clientOption := options.Client().ApplyURI(connectionString)
+
+	//connect to mogodb
+	client, err := mongo.Connect(context.TODO(), clientOption)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("MongoDB connection success")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	//collection instance
+	fmt.Println("Collection instance is ready")
+
+}
+
+// MONGODB helpers - file
+
+// insert 1 record
+
+func insertOneMovie(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted 1 movie in db with id: ",inserted.InsertedID)
+
+}
