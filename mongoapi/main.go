113583b50f915b488fe78c23567bb7ab9700cd34
Tanmoy037 <tanmoysantra67@gmail.com>
2023-10-12 18:04:57 +0530

    port added to serve the route

--- mongoapi/main.go
+++ mongoapi/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Tanmoy037/mongoapi/router"
+)
 
 func main() {
 	fmt.Println("This Go-API")
+	r := router.Router()
+	fmt.Println("Server is getting started...")
+	log.Fatal(http.ListenAndServe(":4000", r))
+	fmt.Println("Listen at port 4000 ...")
+
 }
