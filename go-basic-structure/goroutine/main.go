00b38683b80aa105d6ac3b6241068594344b5c07
Tanmoy037 <tanmoysantra67@gmail.com>
2023-10-15 12:00:02 +0530

    sync package for goroutine added

--- go-basic-structure/goroutine/main.go
+++ go-basic-structure/goroutine/main.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
 
+var wg sync.WaitGroup
+
 func main() {
-	go greeter("Hello")
-	greeter("World")
+	// go greeter("Hello")
+	// greeter("World")
+
+	websitelist := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://twitter.com",
+		"https://instagram.com",
+		"https://github.com",
+	}
+	for _, web := range websitelist {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+	wg.Wait()
+
 
 }
 
-func greeter(s string) {
-	for i := 0; i < 6; i++ {
-		time.Sleep(3 * time.Millisecond)
-		fmt.Println(s)
+
+
+// func greeter(s string) {
+// 	for i := 0; i < 6; i++ {
+// 		time.Sleep(3 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+
+	res, err := http.Get(endpoint)
+
+	if err != nil {
+		println("OOPS error in the wesite")
 	}
+	fmt.Printf("%d status code for %s\n", res.StatusCode, endpoint)
+
 }
